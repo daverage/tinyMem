c5d9f67e55fa9b8a6d862de1b7582db1031f67b7
Andrzej Marczewski <daverage@gmail.com>
2026-01-28 23:03:53 +0000

    Release v0.1.7

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,4 +2,4 @@ package version
 
 // Version is the current version of tinyMem
 // This can be overridden at build time using -ldflags "-X github.com/andrzejmarczewski/tinyMem/internal/version.Version=vX.Y.Z"
-var Version = "v0.1.6"
+var Version = "v0.1.7"
